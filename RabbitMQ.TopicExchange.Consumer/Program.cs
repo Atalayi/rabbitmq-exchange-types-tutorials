8df89ddda973ea8f2c32c75fc0fbccb0a45617fb
Semih Atalay <semihatalay23@outlook.com>
2024-05-29 19:26:16 +0300

    topic exchange add

---
+++ RabbitMQ.TopicExchange.Consumer/Program.cs
@@ -0,0 +1,29 @@
+﻿
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+ConnectionFactory factory = new();
+factory.Uri = new("amqps://vwxwdwdh:DVMos4xd8TUPLFQK1N8E4SZ1EjCi9Jhc@chimpanzee.rmq.cloudamqp.com/vwxwdwdh");
+
+using IConnection connection = factory.CreateConnection();
+using IModel channel = connection.CreateModel();
+
+channel.ExchangeDeclare(exchange: "topic-exchange-example", type: ExchangeType.Topic);
+
+Console.Write("Specify the topic format to be listened to: ");
+string topic = Console.ReadLine();
+
+string queueName = channel.QueueDeclare().QueueName;
+channel.QueueBind(queue: queueName, exchange: "topic-exchange-example", routingKey: topic);
+
+EventingBasicConsumer consumer = new(channel);
+channel.BasicConsume(queue: queueName, autoAck: true, consumer);
+
+consumer.Received += (sender, e) =>
+{
+    string message = Encoding.UTF8.GetString(e.Body.Span);
+    Console.WriteLine(message);
+};
+
+Console.Read();
---
+++ RabbitMQ.TopicExchange.Publisher/Program.cs
@@ -0,0 +1,24 @@
+﻿
+using RabbitMQ.Client;
+using System.Text;
+
+ConnectionFactory factory = new();
+factory.Uri = new("amqps://vwxwdwdh:DVMos4xd8TUPLFQK1N8E4SZ1EjCi9Jhc@chimpanzee.rmq.cloudamqp.com/vwxwdwdh");
+
+using IConnection connection = factory.CreateConnection();
+using IModel channel = connection.CreateModel();
+
+channel.ExchangeDeclare(exchange: "topic-exchange-example", type: ExchangeType.Topic);
+
+for (int i = 0; i < 100; i++)
+{
+    await Task.Delay(200);
+    byte[] message = Encoding.UTF8.GetBytes($"Hello {i}");
+
+    Console.Write("Specify the topic format in which the message will be sent : ");
+    string topic = Console.ReadLine();
+
+    channel.BasicPublish(exchange: "topic-exchange-example", routingKey: topic, body: message);
+}
+
+Console.Read();
