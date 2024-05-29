889c73b8b1cfee3815d9d151a63957a0909f7c86
Semih Atalay <semihatalay23@outlook.com>
2024-05-29 18:53:44 +0300

    direct exchange add

---
+++ RabbitMQ.DirectExchange.Consumer/Program.cs
@@ -0,0 +1,29 @@
+﻿
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+ConnectionFactory factory = new();
+factory.Uri = new("amqps://vwxwdwdh:DVMos4xd8TUPLFQK1N8E4SZ1EjCi9Jhc@chimpanzee.rmq.cloudamqp.com/vwxwdwdh");
+
+using IConnection connection = factory.CreateConnection();
+using IModel channel = connection.CreateModel();
+
+channel.ExchangeDeclare(exchange: "direct-exchange-example", type: ExchangeType.Direct);
+
+string queueName = channel.QueueDeclare().QueueName;
+
+channel.QueueBind(queue: queueName, exchange: "direct-exchange-example", routingKey: "direct-queue-example");
+
+EventingBasicConsumer consumer = new EventingBasicConsumer(channel);
+channel.BasicConsume(queue: queueName, autoAck: true, consumer: consumer);
+
+consumer.Received += (sender, e) =>
+{
+    string message = Encoding.UTF8.GetString(e.Body.Span);
+    Console.WriteLine(message);
+};
+
+Console.Read();
+
+
---
+++ RabbitMQ.DirectExchange.Publisher/Program.cs
@@ -0,0 +1,22 @@
+﻿
+using RabbitMQ.Client;
+using System.Text;
+
+ConnectionFactory factory = new();
+factory.Uri = new("amqps://vwxwdwdh:DVMos4xd8TUPLFQK1N8E4SZ1EjCi9Jhc@chimpanzee.rmq.cloudamqp.com/vwxwdwdh");
+
+using IConnection connection = factory.CreateConnection();
+using IModel channel = connection.CreateModel();
+
+channel.ExchangeDeclare(exchange: "direct-exchange-example", type: ExchangeType.Direct);
+
+while (true)
+{
+    Console.Write("Message : ");
+    string? message = Console.ReadLine();
+    byte[] byteMessage = Encoding.UTF8.GetBytes(message ?? string.Empty);
+
+    channel.BasicPublish(exchange: "direct-exchange-example", routingKey: "direct-queue-example", body: byteMessage);
+}
+
+Console.Read();
